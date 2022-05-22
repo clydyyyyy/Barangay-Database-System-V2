579b8a00431efadd9760cc551bc4496a08acefd9
cloudiana <almightrule@gmail.com>
2022-05-22 17:29:42 +0800

    Version 2

---
+++ Barangay_Household_System/Module/Cipher.vb
@@ -0,0 +1,31 @@
+﻿Module Cipher
+    Function Caesar_Cipher(ByVal Text As String, ByVal N As Integer, ByVal Encryption As Boolean)
+        Dim Result As String = ""
+
+        For Each c As Char In Text
+            Dim x As Integer
+            If Encryption Then
+                x = Asc(c) + N
+            Else
+                x = Asc(c) + 26 - N
+            End If
+
+            If Char.IsLower(c) Then
+                If x > Asc("z") Then
+                    x -= 26
+                End If
+                Result += Chr(x)
+            ElseIf Char.IsUpper(c) Then
+                If x > Asc("Z") Then
+                    x -= 26
+                End If
+                Result += Chr(x)
+            Else
+                Result += c
+            End If
+
+        Next
+
+        Return Result
+    End Function
+End Module
---
+++ Barangay_Household_System/Users/user_info_edit.vb
@@ -0,0 +1,14 @@
+﻿Public Class user_info_edit
+    Private Sub log_btn_Click(sender As Object, e As EventArgs) Handles log_btn.Click
+        Try
+            'update_data("UPDATE `user` SET `username`='" & user_tb.Text & "',`password`='" & pass_tb.Text & "' WHERE `official_id`='" & user_brgy_id.Text & "'")
+            update_data("UPDATE `user` SET `username`='" & Caesar_Cipher(user_tb.Text, 2, True) & "',`password`='" & Caesar_Cipher(pass_tb.Text, 2, True) & "' WHERE `username`='" & Caesar_Cipher(user_tb.Text, 2, True) & "' OR `password`='" & Caesar_Cipher(pass_tb.Text, 2, True) & "'")
+
+            Me.Close()
+            connection.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
